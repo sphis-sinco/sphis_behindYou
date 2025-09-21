b54cf2b1ab4752b0f931052bb801a70920aa55bc
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-21 15:11:26 -0500

    chanceOfCharacterEntering

--- source/sphis/behindYou/Scene.hx
+++ source/sphis/behindYou/Scene.hx
@@ -4,14 +4,17 @@ class Scene
 {
 	public var id:String;
 
-	public function new(id:String)
+	public var chance_of_character_entering:Float;
+
+	public function new(id:String, ?chance_of_character_entering:Float = 50)
 	{
 		this.id = id;
+		this.chance_of_character_entering = chance_of_character_entering;
 	}
 
 	public function clone():Scene
 	{
-		return new Scene(this.id);
+		return new Scene(this.id, this.chance_of_character_entering);
 	}
 
 	public function update(elapsed:Float) {}
